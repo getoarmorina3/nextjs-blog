f2a3dd18a657a3714e52148f977c9d3155f86ae4
getoarmorina3 <getoaarm1@gmail.com>
2023-12-08 02:29:55 +0100

    remove git provider

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,29 +1,29 @@
-'use client'
+"use client";
 
-import { cn } from '../lib/utils'
-import { signIn } from 'next-auth/react'
-import * as React from 'react'
-import { FC } from 'react'
-import { Button } from './ui/button'
-import { useToast } from './ui/use-toast'
-import { Icons } from './ui/icons'
+import { cn } from "../lib/utils";
+import { signIn } from "next-auth/react";
+import * as React from "react";
+import { FC } from "react";
+import { Button } from "./ui/button";
+import { useToast } from "./ui/use-toast";
+import { Icons } from "./ui/icons";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
-  const { toast } = useToast()
-  const [isLoading, setIsLoading] = React.useState<boolean>(false)
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
-  const loginWith = async (provider: string) => {
+  const loginWithGoogle = async () => {
     setIsLoading(true);
 
     try {
-      await signIn(provider);
+      await signIn('google');
     } catch (error) {
       toast({
-        title: 'Error',
-        description: `There was an error logging in with ${provider}`,
-        variant: 'destructive',
+        title: "Error",
+        description: `There was an error logging in with google`,
+        variant: "destructive",
       });
     } finally {
       setIsLoading(false);
@@ -31,30 +31,23 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
   };
 
   return (
-    <div className={cn('flex  flex-col justify-center gap-4', className)} {...props}>
+    <div
+      className={cn("flex  flex-col justify-center gap-4", className)}
+      {...props}
+    >
       <Button
-      variant={'outline'}
         isLoading={isLoading}
-        type='button'
-        size='sm'
-        className='w-full'
-        onClick={() => loginWith('google')}
-        disabled={isLoading}>
-        {isLoading ? null : <Icons.google className='h-4 w-4 mr-2' />}
+        type="button"
+        size="sm"
+        className="w-full"
+        onClick={loginWithGoogle}
+        disabled={isLoading}
+      >
+        {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
         Google
       </Button>
-      <Button
-        isLoading={isLoading}
-        type='button'
-        size='sm'
-        className='w-full'
-        onClick={() => loginWith('github')}
-        disabled={isLoading}>
-        {isLoading ? null : <Icons.gitHub className='h-4 w-4 mr-2' />}
-        Github
-      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default UserAuthForm
+export default UserAuthForm;
