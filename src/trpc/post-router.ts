9eedcd0ff2167b93b7512b5916f3ea6523083a03
getoarmorina3 <getoaarm1@gmail.com>
2023-12-15 16:25:03 +0100

    Define post router with tRPC
    Co-Authored-By: elsaukajj <120333700+elsaukajj@users.noreply.github.com>

--- src/trpc/post-router.ts
+++ src/trpc/post-router.ts
@@ -4,7 +4,8 @@ import slugify from "slugify";
 import { db } from "@/lib/db";
 
 export const postRouter = router({
-  list: publicProcedure.query(() => {
+  // list all posts in descending order including the author
+  listAll: publicProcedure.query(() => {
     return db.post.findMany({
       orderBy: {
         createdAt: "desc",
@@ -14,11 +15,35 @@ export const postRouter = router({
       },
     });
   }),
+  // include the author and only return the posts of the current user
+  myBlogs: protectedProcedure.query(({ ctx }) => {
+    const userId = ctx.user!.id;
+
+    return db.post.findMany({
+      where: {
+        authorId: userId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        author: true,
+      },
+    });
+  }),
+  // only the authenticated users can create a post
   create: protectedProcedure
     .input(
       z.object({
-        title: z.string(),
-        content: z.string(),
+        title: z
+          .string()
+          .min(3, {
+            message: "Title must be at least 3 characters long",
+          })
+          .max(72, {
+            message: "Title must be less than 72 characters long",
+          }),
+        content: z.any(),
       })
     )
     .mutation(async ({ ctx, input }) => {
@@ -36,6 +61,7 @@ export const postRouter = router({
 
       return payload;
     }),
+  // view a single post by slug
   view: publicProcedure
     .input(
       z.object({
@@ -47,6 +73,55 @@ export const postRouter = router({
         where: {
           slug: opts.input.slug,
         },
+        include: {
+          author: {
+            select: {
+              id: true,
+              name: true,
+              image: true,
+            },
+          },
+        },
       });
+    }),
+  // delete a post by slug only the owner or the admin can delete
+  delete: protectedProcedure
+    .input(
+      z.object({
+        slug: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id, role } = ctx!.user;
+
+      // Find the post by ID
+      const post = await db.post.findUnique({
+        where: {
+          slug: input.slug,
+        },
+        select: {
+          authorId: true,
+        },
+      });
+
+      if (!post) {
+        throw new Error("Blog not found");
+      }
+
+      // Check if the current user is the owner of the post or is an admin
+      if (role !== "ADMIN" && post.authorId !== id) {
+        throw new Error(
+          "Unauthorized: You are not the owner of this blog or an admin to delete this"
+        );
+      }
+  
+      // Delete the post
+      await db.post.delete({
+        where: {
+          slug: input.slug,
+        },
+      });
+
+      return true;
     }),
 });
