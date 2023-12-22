dd25526510918a584b959aac4dc32c3f7dbaf9d2
getoarmorina3 <getoaarm1@gmail.com>
2023-12-22 13:27:02 +0100

    Update index.ts
    Co-Authored-By: Blinda Behluli <120460025+blindabehluli@users.noreply.github.com>
    Co-Authored-By: elsaukajj <120333700+elsaukajj@users.noreply.github.com>

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,11 +3,13 @@ import z from 'zod';
 import { postRouter } from './routers/post-router';
 import { commentRouter } from './routers/comment-router';
 import { userRouter } from './routers/user-router';
+import { categoryRouter } from './routers/category-router';
 
 export const appRouter = router({
   post: postRouter,
   comment: commentRouter,
   user: userRouter,
+  category: categoryRouter,
   hello: publicProcedure
     .input(
       z.object({
