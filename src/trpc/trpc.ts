fb8e2e45da36fddff984d6fe6a478f0bd00e49ca
getoarmorina3 <getoaarm1@gmail.com>
2023-12-22 13:26:24 +0100

    zod error formater

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,25 +1,41 @@
 import { getAuthSession } from "@/lib/auth";
 import { initTRPC, TRPCError } from "@trpc/server";
+import { ZodError } from "zod";
 
-const t = initTRPC.create()
-const middleware = t.middleware
+const t = initTRPC.create({
+  errorFormatter(opts) {
+    const { shape, error } = opts;
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === "BAD_REQUEST" && error.cause instanceof ZodError
+            ? error.cause.flatten()
+            : null,
+      },
+    };
+  },
+});
+
+const middleware = t.middleware;
 
 const isAuth = middleware(async (opts) => {
-    const { user } = (await getAuthSession()) || {};
+  const { user } = (await getAuthSession()) || {};
 
-    if (!user) {
-        throw new TRPCError({ code: 'UNAUTHORIZED' });
-    }
+  if (!user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
 
-    return opts.next({
-        ...opts,
-        ctx: {
-            ...opts.ctx,
-            user,
-        },
-    });
-})
+  return opts.next({
+    ...opts,
+    ctx: {
+      ...opts.ctx,
+      user,
+    },
+  });
+});
 
-export const router = t.router
-export const publicProcedure = t.procedure
-export const protectedProcedure = t.procedure.use(isAuth)
+export const router = t.router;
+export const publicProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(isAuth);
