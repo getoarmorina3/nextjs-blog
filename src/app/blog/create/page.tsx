a13d9db6b6a52540ccadd78c08e27f44e7e6b2e1
Blinda Behluli <120460025+blindabehluli@users.noreply.github.com>
2023-12-28 10:06:13 +0100

    Update page.tsx

--- src/app/blog/create/page.tsx
+++ src/app/blog/create/page.tsx
@@ -4,15 +4,7 @@ import { Button } from '@/components/ui/button'
 const page = async () => {
   return (
     <div className='flex flex-col items-center gap-6 mt-4 mb-20'>
-      {/* heading */}
-      {/* <div className='py-5'>
-        <div className='-ml-2 -mt-2 flex flex-wrap items-baseline'>
-          <h3 className='ml-2 mt-8 text-4xl font-semibold leading-6'>
-            Create Blog
-          </h3>
-        </div>
-      </div> */}
-
+      
       {/* form */}
       <Editor />
 
